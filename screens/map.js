ca2dde11010d2524d3d40631e73d110bb52742bd
Donghyeon Kim <dkim930@gatech.edu>
2023-04-16 20:39:15 -0400

    Update map.js

--- screens/map.js
+++ screens/map.js
@@ -2,10 +2,18 @@ import React, { useState, useEffect } from 'react';
 import MapView, { Circle } from 'react-native-maps';
 import { Marker } from 'react-native-maps';
 import { StyleSheet, View } from 'react-native';
+import { Text, Pressable } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 import * as Location from 'expo-location';
 
 export default function App() {
+    const navigation = useNavigation();
+
+    const northStar = () => {
+        navigation.navigate("NorthStar");
+    }
+
   const [pin, setPin] = React.useState({
     latitude: 33.7756,  //default location
     longitude: -84.3902,
@@ -33,6 +41,7 @@ export default function App() {
 
   return (
     <View style={styles.container}>
+        
       <MapView style = {styles.map}
         initialRegion={{
           latitude: 33.7756,
@@ -64,10 +73,15 @@ export default function App() {
     center ={pin}
     radius = {50}
     />
-  </MapView>   
+  </MapView>  
+  <TouchableOpacity style={styles.startButton} onPress={map}>
+                <MaterialCommunityIcons name="star-shooting-outline" size={24} color="black" />
+                <Text>Find North Star</Text>
+    </TouchableOpacity> 
   </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -76,5 +90,21 @@ const styles = StyleSheet.create({
   map: {
     width: '100%',
     height: '100%',
+  },
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: 'black',
+  },
+  text: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'white',
   },
 });
